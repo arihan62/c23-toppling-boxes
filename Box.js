b8ac18a47da5f67ca33571a36a459675eb772dec
arihan62 <78147680+arihan62@users.noreply.github.com>
2021-02-18 16:57:33 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            'restitution':0.8, 'friction':0.3, 'density':1.0
+        
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        World.add(world,this.body)
+    }
+    display(){
+        var position=this.body.position
+        rectMode(CENTER)
+        fill(255)
+        rect(position.x,position.y,this.width,this.height)
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c23-toppling-boxes
+# p5.play-boilerplate
+Boiler plate for p5.play
